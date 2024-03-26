b7d60237b4737a78118abaf0a777a2ac622a7263
SOMKEY\ROG <manithjr1331@gmail.com>
2024-03-26 18:55:13 +0700

    complated testing with route using mongo memory server post and get

--- src/routes/__test__/userRoute.test.ts
+++ src/routes/__test__/userRoute.test.ts
@@ -1,7 +1,26 @@
 import request from "supertest";
+import mongoose, { ConnectOptions } from "mongoose";
 import app from "../../index"; // Import your Express application
+import { MongoMemoryServer } from "mongodb-memory-server";
 
 describe("User Routes", () => {
+  let mongoServer: MongoMemoryServer;
+
+  beforeAll(async () => {
+    mongoServer = new MongoMemoryServer();
+    await mongoServer.start(); // Start the server
+    const mongoUri = mongoServer.getUri(); // No need to await here
+    await mongoose.connect(mongoUri, {
+      useUnifiedTopology: true,
+    } as ConnectOptions);
+  });
+
+
+  afterAll(async () => {
+    await mongoose.disconnect();
+    await mongoServer.stop();
+  });
+
   // Test for GET all users route
   describe("GET /", () => {
     it("should respond with status code 200", async () => {
@@ -12,6 +31,16 @@ describe("User Routes", () => {
 
   // Test for POST user route
   describe("POST /user", () => {
-    
-  })
+    it("should create a new user and respond with status code 201", async () => {
+      const userData = {
+        username: "testuser",
+        email: "test@example.com",
+        password: "password123",
+      };
+      const response = await request(app).post("/user").send(userData);
+
+      expect(response.statusCode).toBe(201);
+      // You can add more assertions here if needed
+    });
+  });
 });
